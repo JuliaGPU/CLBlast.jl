36059ca22f0029b82064c8285c8dc7c90fdeaaaa
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-06-26 14:12:40 +0200

    added her2k

---
+++ src/L3/her2k.jl
@@ -0,0 +1,81 @@
+
+for (func, elty, relty) in [(:CLBlastCher2k, Complex64, Float32),
+                            (:CLBlastZher2k, Complex128, Float64)]
+
+    @eval function $func(layout::CLBlastLayout,
+                         triangle::CLBlastTriangle, ab_transpose::CLBlastTranspose,
+                         n::Integer, k::Integer,
+                         alpha::$elty,
+                         a_buffer::cl.CL_mem, a_offset::Integer, a_ld::Integer,
+                         b_buffer::cl.CL_mem, b_offset::Integer, b_ld::Integer,
+                         beta::$relty,
+                         c_buffer::cl.CL_mem, c_offset::Integer, c_ld::Integer,
+                         queue::cl.CmdQueue, event::cl.Event)
+        err = ccall(
+            ($(string(func)), libCLBlast), 
+            cl.CL_int,
+            (Cint, Cint, Cint, Csize_t, Csize_t, $elty, Ptr{Void}, Csize_t, Csize_t, 
+              Ptr{Void}, Csize_t, Csize_t, $elty, Ptr{Void}, Csize_t, Csize_t, Ptr{Void}, Ptr{Void}),
+            Cint(layout), Cint(triangle), Cint(ab_transpose), n, k, alpha,
+              a_buffer, a_offset, a_ld, b_buffer, b_offset, b_ld, beta, c_buffer, c_offset, c_ld, Ref(queue), Ref(event)
+        )
+        if err != cl.CL_SUCCESS
+            println(STDERR, "Calling function $(string($func)) failed!")
+            throw(CLBlastError(err))
+        end
+        return err
+    end
+
+    @eval function her2k!(uplo::Char, trans::Char, α::Number, A::cl.CLArray{$elty,2},
+                          B::cl.CLArray{$elty,2}, β::Number, C::cl.CLArray{$elty,2};
+                          queue::cl.CmdQueue=cl.queue(y))
+        # check and convert arguments
+        if trans == 'N'
+            ab_transpose = CLBlastTransposeNo
+        elseif trans == 'T'
+            ab_transpose = CLBlastTransposeYes
+        else
+            throw(ArgumentError("Transpose marker `trans` is $(trans) but only 'N' and 'T' are allowed."))
+        end
+        if uplo == 'U'
+            triangle = CLBlastTriangleUpper
+        elseif uplo == 'L'
+            triangle = CLBlastTriangleLower
+        else
+            throw(ArgumentError("Upper/lower marker `uplo` is $(uplo) but only 'U' and 'L' are allowed."))
+        end
+        n = size(C,1)
+        if n != size(C,2)
+            throw(DimensionMismatch("`C` has dimensions $(size(C)) but must be square."))
+        end
+        nn = size(A, trans == 'N' ? 1 : 2)
+        if nn != n 
+            throw(DimensionMismatch("`C` has size ($n,$n), corresponding dimension of `A` is $nn.")) 
+        end
+        if size(A) != size(B)
+            throw(DimensionMismatch("`A` has size $(size(A)), `B` has size $(size(B)).")) 
+        end
+        k  = size(A, trans == 'N' ? 2 : 1)
+        alpha = convert($elty, α)
+        beta  = convert($relty, β)
+        layout = CLBlastLayoutColMajor
+
+        # output event
+        event = cl.Event(C_NULL)
+
+        $func(layout, triangle, ab_transpose,
+              n, k,
+              alpha,
+              pointer(A), 0, size(A,1),
+              pointer(B), 0, size(B,1),
+              beta,
+              pointer(C), 0, size(C,1),
+              queue, event)
+
+        # wait for kernel
+        cl.wait(event)
+
+        nothing
+    end
+
+end
--- test/L3_test.jl
+++ test/L3_test.jl
@@ -319,6 +319,55 @@ end
 @testset "syr2k!" begin 
     for elty in elty_L1
         is_linux() && elty == Complex64 && continue
+
+        # A*B'
+        A = rand(elty, n_L3, k_L3)
+        A_cl = cl.CLArray(queue, A)
+        B = rand(elty, n_L3, k_L3)
+        B_cl = cl.CLArray(queue, B)
+        C = rand(elty, n_L3, n_L3)
+        C_cl = cl.CLArray(queue, C)
+        α = rand(elty)
+        β = rand(elty)
+
+        for uplo in ['U','L']
+            CLBlast.syr2k!(uplo, 'N', α, A_cl, B_cl, β, C_cl, queue=queue)
+            LinAlg.BLAS.syr2k!(uplo, 'N', α, A, B, β, C)
+            @test cl.to_host(A_cl, queue=queue) ≈ A
+            @test cl.to_host(B_cl, queue=queue) ≈ B
+            @test cl.to_host(C_cl, queue=queue) ≈ C
+
+            @test_throws DimensionMismatch CLBlast.syr2k!(uplo, 'T', α, A_cl, B_cl, β, C_cl, queue=queue)
+        end
+
+        @test_throws ArgumentError CLBlast.syr2k!('A', 'N', α, A_cl, B_cl, β, C_cl, queue=queue)
+        @test_throws ArgumentError CLBlast.syr2k!('U', 'A', α, A_cl, B_cl, β, C_cl, queue=queue)
+
+        # A'*B
+        A = rand(elty, k_L3, n_L3)
+        A_cl = cl.CLArray(queue, A)
+        B = rand(elty, k_L3, n_L3)
+        B_cl = cl.CLArray(queue, B)
+        C = rand(elty, n_L3, n_L3)
+        C_cl = cl.CLArray(queue, C)
+        α = rand(elty)
+        β = rand(elty)
+
+        for uplo in ['U','L']
+            CLBlast.syr2k!(uplo, 'T', α, A_cl, B_cl, β, C_cl, queue=queue)
+            LinAlg.BLAS.syr2k!(uplo, 'T', α, A, B, β, C)
+            @test cl.to_host(A_cl, queue=queue) ≈ A
+            @test cl.to_host(B_cl, queue=queue) ≈ B
+            @test cl.to_host(C_cl, queue=queue) ≈ C
+
+            @test_throws DimensionMismatch CLBlast.syr2k!(uplo, 'N', α, A_cl, B_cl, β, C_cl, queue=queue)
+        end
+    end
+end
+
+@testset "her2k!" begin 
+    for elty in elty_L1
+        is_linux() && elty == Complex64 && continue
         elty <: Complex || continue
 
         # A*B'
