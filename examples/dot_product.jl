b3f184ae370658be75ccc66fd658cbb75f83af4f
Hendrik Ranocha <h.ranocha@tu-bs.de>
2018-08-14 13:29:24 +0200

    added examples for dot and nrm2

---
+++ examples/dot_product.jl
@@ -0,0 +1,46 @@
+using OpenCL, CLBLAS, CLBlast, BenchmarkTools
+
+CLBLAS.setup()
+
+function run_dot(n::Integer, T=Float32::Union{Type{Float32},Type{Float64}})
+    srand(12345)
+    y = rand(T, n)
+    x = rand(T, n)
+
+    @printf("\nn = %d, eltype = %s\n", n, T)
+
+    println("BLAS:")
+    res_true = LinAlg.BLAS.dot(n, x, 1, y, 1)
+    display(@benchmark LinAlg.BLAS.dot($n, $x, $1, $y, $1))
+    println()
+
+    for device in cl.devices()
+        println("-"^70)
+        @printf("Platform name   : %s\n", device[:platform][:name])
+        @printf("Platform version: %s\n", device[:platform][:version])
+        @printf("Device name     : %s\n", device[:name])
+        @printf("Device type     : %s\n", device[:device_type])
+        println()
+
+        ctx = cl.Context(device)
+        queue = cl.CmdQueue(ctx)
+
+        println("CLBLAS:")
+        y_cl = cl.CLArray(queue, y)
+        x_cl = cl.CLArray(queue, x)
+        res_cl = CLBLAS.dot(n, x_cl, 1, y_cl, 1)
+        @assert res_cl ≈ res_true
+        display(@benchmark CLBLAS.dot($n, $x_cl, $1, $y_cl, $1))
+        println()
+
+        println("CLBlast:")
+        y_cl = cl.CLArray(queue, y)
+        x_cl = cl.CLArray(queue, x)
+        res_cl = CLBlast.dot(n, x_cl, 1, y_cl, 1)
+        @assert res_cl ≈ res_true
+        display(@benchmark CLBlast.dot($n, $x_cl, $1, $y_cl, $1))
+        println()
+    end
+end
+
+run_dot(2^16, Float32)
---
+++ examples/euclidean_norm.jl
@@ -0,0 +1,43 @@
+using OpenCL, CLBLAS, CLBlast, BenchmarkTools
+
+CLBLAS.setup()
+
+function run_nrm2(n::Integer, T=Float32::Union{Type{Float32},Type{Float64}})
+    srand(12345)
+    x = rand(T, n)
+
+    @printf("\nn = %d, eltype = %s\n", n, T)
+
+    println("BLAS:")
+    res_true = LinAlg.BLAS.nrm2(n, x, 1)
+    display(@benchmark LinAlg.BLAS.nrm2($n, $x, $1))
+    println()
+
+    for device in cl.devices()
+        println("-"^70)
+        @printf("Platform name   : %s\n", device[:platform][:name])
+        @printf("Platform version: %s\n", device[:platform][:version])
+        @printf("Device name     : %s\n", device[:name])
+        @printf("Device type     : %s\n", device[:device_type])
+        println()
+
+        ctx = cl.Context(device)
+        queue = cl.CmdQueue(ctx)
+
+        println("CLBLAS:")
+        x_cl = cl.CLArray(queue, x)
+        res_cl = CLBLAS.nrm2(n, x_cl, 1)
+        @assert res_cl ≈ res_true
+        display(@benchmark CLBLAS.nrm2($n, $x_cl, $1))
+        println()
+
+        println("CLBlast:")
+        x_cl = cl.CLArray(queue, x)
+        res_cl = CLBlast.nrm2(n, x_cl, 1)
+        @assert res_cl ≈ res_true
+        display(@benchmark CLBlast.nrm2($n, $x_cl, $1))
+        println()
+    end
+end
+
+run_nrm2(2^16, Float32)
